4cc3eb954d887c8ab94451e1da8f7e6b6847e55b
tsl-ramkrishna <ram_krishna.shrestha@tsl.ac.uk>
2020-09-02 09:23:47 +0000

    docker image of bash script for bowtie2 alignment

---
+++ bowtie2alignment/bowtie2alignment.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+usage()
+{
+	echo "Bash program to align short paired end reads to a reference sequence using bowtie2"
+	echo "----------------------------------------------------------------------------------"
+	echo "Usage
+		-s	sample name
+		-r	reference fasta file
+		-1	forward reads R1
+		-2	reverse reads R2
+		-o	output sorted bam filename"
+}
+
+while getopts s:r:1:2:o:h option
+do
+	case "$option" in
+		s) sample=$OPTARG;;
+		r) reference=$OPTARG;;
+		1) R1=$OPTARG;;
+		2) R2=$OPTARG;;
+		o) output=$OPTARG;;
+		h) usage;;
+	esac
+
+done
+
+echo "You supplied following :"
+echo "Sample name "$sample
+echo Reference fasta file $reference
+echo Forward reads file $R1
+echo Reverse reads file $R2
+echo Output bam file name $output
+
+
+echo Building the reference index now
+
+bowtie2-build -f $reference
+echo Building reference index completed.
+
+echo Running bowtie2 alignment now
+
+bowtie2  --no-discordant  --no_unl --rg-id $sample --rg SM:$sample --rg LB:NA --rg PL:Illumina  -x $reference -1 ${R1} -2 ${R2} | samtools view -hbS | samtools sort -o ${output} && samtools index ${output}
